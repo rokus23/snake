08a3e21c57f4db497b66a3258254150777dbf801
rokus23 <88277064+rokus23@users.noreply.github.com>
2021-08-02 12:14:33 +0200

    Van alma amit a kígyóval meg lehet szerezni. Nem lehet kimenni a pályáról, mert akkor vesztesz. Pontokat is számol.

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     irany = -1
 })
+input.onButtonPressed(Button.AB, function () {
+    control.reset()
+})
 input.onButtonPressed(Button.B, function () {
     irany = 1
 })
@@ -10,6 +13,12 @@ let irany = 0
 let x = 2
 let y = 2
 irany = 1
+let apple = randint(0, 4)
+while (apple == x) {
+    apple = randint(0, 4)
+}
+basic.pause(500)
+led.plot(apple, 2)
 basic.forever(function () {
     led.plot(x, y)
     elozoY = y
@@ -17,7 +26,17 @@ basic.forever(function () {
     basic.pause(1000)
     x += irany
     led.unplot(elozoX, elozoY)
+    if (apple == x) {
+        game.addScore(1)
+        apple = randint(0, 4)
+        while (apple == x) {
+            apple = randint(0, 4)
+        }
+        basic.pause(500)
+        led.plot(apple, 2)
+    }
     if (x == 5 || x == -1) {
         basic.showIcon(IconNames.Sad)
+        basic.showNumber(game.score())
     }
 })
